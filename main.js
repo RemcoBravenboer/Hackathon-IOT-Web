b3641e5deb075bc094c73149ed2c753105b0e8e7
Remco Bravenboer <r.bravenboer1996@gmail.com>
2019-09-04 20:00:19 +0200

    changes

--- main.js
+++ main.js
@@ -1,5 +1,12 @@
-//onload function
-(function() {
+function loadPanel(x) {
+	var n = document.getElementById("panelNotifications");
+	var h = document.getElementById("panelHistory");
 
-
-})();
+	if(x == "notifications") {
+		h.style.display = "none";
+		n.style.display = "block";
+	} else {
+		n.style.display = "none";
+		h.style.display = "block";
+	}
+}
