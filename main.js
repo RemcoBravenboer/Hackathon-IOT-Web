cac9aeab719cd6298246c7b4fb6130ce8e6bdbdf
Mona Ullah <monaul@stud.ntnu.no>
2019-09-05 10:36:39 +0200

    menu stuff

--- main.js
+++ main.js
@@ -1,12 +1,18 @@
 function loadPanel(x) {
 	var n = document.getElementById("panelNotifications");
 	var h = document.getElementById("panelHistory");
+	var menuN = document.getElementById("menuN");
+    var menuH = document.getElementById("menuH");
 
 	if(x == "notifications") {
 		h.style.display = "none";
 		n.style.display = "block";
+		menuN.style.fontWeight = "bold";
+        menuH.style.fontWeight = "normal";
 	} else {
 		n.style.display = "none";
 		h.style.display = "block";
-	}
+        menuH.style.fontWeight = "bold";
+        menuN.style.fontWeight = "normal";
+    }
 }
